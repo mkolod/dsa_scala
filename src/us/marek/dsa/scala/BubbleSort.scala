21de1043a9eb3beb39f33396309e1f803a283163
Marek Kolodziej <mkolod@gmail.com>
2013-08-24 19:50:09 -0400

    Minor reformatting

--- src/us/marek/dsa/scala/BubbleSort.scala
+++ src/us/marek/dsa/scala/BubbleSort.scala
@@ -2,14 +2,12 @@ package us.marek.dsa.scala
 
 import scala.util.Random
 
-
 object BubbleSort extends App {
   
   val randomList = createRandomData(10)
   println(s"Unsorted list:\n$randomList\n")
   printf("Sorted list (bubble sort):\n%s\n", bubbleSort(randomList, randomList.length))
 
- 
   def createRandomData(numEl: Int) = Array.fill(numEl)(Random.nextDouble).toList
   def sort(a: List[Double], b: Double) = if (a.last <  b) a ++ List(b) else a.init ++ List(b) ++ List(a.last)
   def bubbleStep(l: List[Double]) = l.sliding(1).reduceLeft((a,b) => sort(a, b(0)))   
