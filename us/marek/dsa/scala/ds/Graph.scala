65caab936a36ec09d2a7af7e1c22b4dda2263ca1
Marek Kolodziej <mkolod@gmail.com>
2014-01-13 07:47:24 -0500

    Abstraction improvements to graphs

--- us/marek/dsa/scala/ds/Graph.scala
+++ us/marek/dsa/scala/ds/Graph.scala
@@ -1,74 +1,123 @@
 package us.marek.dsa.scala.ds
-//package graph
 
-import scala.collection.mutable.{ HashMap, HashSet, Stack => CStack }
-import scala.reflect.ClassTag
+// needed to rename Stack since I also have my own implementation in this package
+import scala.collection.mutable.{ HashMap, HashSet, LinkedHashSet, Stack => CStack }
 
+
+// let's test the code
 object Graph extends App {
 
   implicit def stringToVertex(s: String) = new Vertex(s)
-  val graph = new Graph[String, Option[Int]]()
   
-  "A,B,C,D,E,F,G,H,I".split(",").foreach(s => graph.addVertex(s))
-  graph.addUndirectedEdge("A", "B", None)
-  graph.addUndirectedEdge("B", "F", None)
-  graph.addUndirectedEdge("F", "H", None)
-  graph.addUndirectedEdge("A", "C", None)
-  graph.addUndirectedEdge("A", "D", None)
-  graph.addUndirectedEdge("D", "G", None)
-  graph.addUndirectedEdge("G", "I", None)
-  graph.addUndirectedEdge("A", "B", None)
-  graph.addUndirectedEdge("A", "E", None)
+  // some test data 
   
-  graph.dfs(Vertex("A"), f = s => println(s"visiting $s"))
- 
-  /*
- "Ajo,Bordo,Colina,Danza,Erizo,Flor".split(",").foreach(s => graph.addVertex(s))
-  graph.addUndirectedEdge("Ajo", "Bordo", 6)
-  graph.addUndirectedEdge("Ajo", "Danza", 4)
-  graph.addUndirectedEdge("Bordo", "Danza", 7)
-  graph.addUndirectedEdge("Bordo", "Colina", 10)
-  graph.addUndirectedEdge("Bordo", "Erizo", 7)
-  graph.addUndirectedEdge("Danza", "Erizo", 12)
-  graph.addUndirectedEdge("Danza", "Colina", 8)
-  graph.addUndirectedEdge("Colina", "Erizo", 5)
-  graph.addUndirectedEdge("Colina", "Flor", 6)
-  graph.addUndirectedEdge("Erizo", "Flor", 7)
+  val graph = new Graph[String, Option[Int]]() {
+    "A,B,C,D,E,F,G,H,I".split(",").foreach(s => addVertex(s))
+    addUndirectedEdge("A", "B", None)  // we don't care about the weights for now, so weights = None
+    addUndirectedEdge("B", "F", None)
+    addUndirectedEdge("F", "H", None)
+    addUndirectedEdge("A", "C", None)
+    addUndirectedEdge("A", "D", None)
+    addUndirectedEdge("D", "G", None)
+    addUndirectedEdge("G", "I", None)
+    addUndirectedEdge("A", "E", None)
+  }
   
-  graph.dfs(Vertex("Ajo"), f = s => println(s"visiting $s"))
-  */
+  /* test depth-first and breadth-first search, so far, the implementation
+     is a traversal, not a search, but that will change*/
+  
+  val start = Vertex("A")
+  val f = (v: Vertex[String]) => println(s"visiting $v")
+  
+  println("\nDepth-first search\n")
+  graph.depthFirstSearch(start, f)
+  
+  println("\nBreadth-first search\n")
+  graph.breadthFirstSearch(start, f)
+} // end object Graph
 
+
+/* collections for search - unify API
+   the logic - DFS and BFS work exactly the same way,
+   but DFS uses a stack while BFS uses a queue,
+   so we can "inject" the need collection and write
+   the search once 
+   note that for Dijkstra's algorithm, we will need
+   a priority queue/heap, so if we make the method 
+   general enough, all we'll need to do is provide
+   the correct data structure (in Dijkstra's case, 
+   path will matter, too)
+ */
+sealed abstract trait GraphCollection[T] {
+  def add(t: T): Unit
+  def remove: Option[T]
+  def peek: Option[T]
+  def isEmpty: Boolean
 }
 
+case class GraphQueue[T] extends GraphCollection[T] {
+  private val q = new scala.collection.mutable.Queue[T]()
+  override def add(t: T) = q.enqueue(t)
+  override def remove = if (!q.isEmpty) Some(q.dequeue) else None
+  override def peek = if (!q.isEmpty) Some(q.front) else None
+  override def isEmpty = q.isEmpty
+  override def toString = s"GraphQueue(${q.toList.mkString(",")})"
+}
+  
+case class GraphStack[T] extends GraphCollection[T] {
+  private val s = new scala.collection.mutable.Stack[T]()
+  override def add(t: T) = s.push(t)
+  override def remove = if (!s.isEmpty) Some(s.pop) else None
+  override def peek = if (!s.isEmpty) Some(s.top) else None
+  override def isEmpty = s.isEmpty
+  override def toString = s"GraphStack(${s.toList.mkString(",")})"
+} 
+// end collection abstraction
 
 
+/* let's get equals/hashCode/toString etc.
+ get rid of type erasure and restrict to ordered types (e.g. for topo sorting, etc.) */
 case class Vertex[T <% Ordered[T]: Manifest](value: T)
 
-case class Edge[T <% Ordered[T]: Manifest, S <% Ordered[S]: Manifest](from: Vertex[T], to: Vertex[T], weight: Option[S] = None) extends Ordered[Edge[T, S]] {
 
+/* graph edges, get rid of type erasure, require ordering since vertices are ordered
+   may need that for topological sorting, etc. */
+case class Edge[T <% Ordered[T]: Manifest, S <% Ordered[S]: Manifest]
+  (from: Vertex[T], to: Vertex[T], weight: Option[S] = None) extends Ordered[Edge[T, S]] {
+
+  // needed for the Ordered view bound
   def compare(that: Edge[T, S]): Int = (this.weight, that.weight) match {
     case (Some(x), Some(y)) => if (x == y) 0 else if (x < y) 1 else -1
     case _ => throw new UnsupportedOperationException("Can't compare edges with no weights")
   }
 
+  // ignore weight for equals, since some graphs will be unweighted
   override def equals(that: Any) = that match {
     case Edge(f, t, _) => this.from == f && this.to == t
     case _ => false
   }
 
-}
+} // end class Edge
 
+
+// finally, the main graph class !!!
 class Graph[T <% Ordered[T]: Manifest, S <% Ordered[S]: Manifest] {
 
-  private var numVert = 0
-
-  type AdjHash = HashMap[Vertex[T], HashSet[Edge[T, S]]]
+  /* I came up with the hash table implementation of the adjacency "matrix" myself,
+    but apparently Guido van Rossum is a big promoter of this for adjacency lists:
+    http://www.python.org/doc/essays/graphs/
+    LinkedHashSet isn't critical (a regular HashSet will do), but it will keep 
+    the insertion order to easily compare against test samples
+    let's make it a type in case we need to use it in other places */
+  type AdjHash = HashMap[Vertex[T], LinkedHashSet[Edge[T, S]]]
   val adjHash = new AdjHash()
 
-  def addVertex(v: Vertex[T]) = {
-    adjHash.put(v, HashSet.empty[Edge[T, S]])
-    numVert += 1
-  }
+  /* the value will be an empty set until we add data, but the vertex will be invisible 
+     until it appears in the adjacency hash table*/
+  def addVertex(v: Vertex[T]) = adjHash.put(v, LinkedHashSet.empty[Edge[T, S]])
+  
+  
+  // to do - delete vertex !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   /*
   def deleteVertex[S](v: Vertex[T]) = {
@@ -96,61 +145,41 @@ class Graph[T <% Ordered[T]: Manifest, S <% Ordered[S]: Manifest] {
     val e = Edge(from, to, Some(weight))
     adjHash.get(from).get.add(e)
   }
-
-  def dfs(start: Vertex[T], f: Vertex[T] => Unit): Unit = {
-    
-    println("In dfs")
+  
+  // abstraction = :)
+  def depthFirstSearch(start: Vertex[T], f: Vertex[T] => Unit) = search(start, f, new GraphStack[Vertex[T]]())
+  def breadthFirstSearch(start: Vertex[T], f: Vertex[T] => Unit) = search(start, f, new GraphQueue[Vertex[T]]())
+  
+  // accept different data structures (stack, queue) and genetate different search behavior (DFS, BFS)
+  private def search(start: Vertex[T], f: Vertex[T] => Unit, collection: GraphCollection[Vertex[T]]) = {
+    println("In search... :))")
     require(adjHash.contains(start), s"starting vertex $start not in graph")
-    val s = new CStack[Vertex[T]]()
+    
     val visited = new HashSet[Vertex[T]]()
     
-    def getUnvisited(h: HashSet[Edge[T,S]]) = h.find(v => !visited.contains(v.to))
-      /*
-       
-       adjHash.get(s.top) match {
-      case Some(x) => x.dropWhile(y => visited.contains(y.to)).take(1).toSeq(0)
-      case None => None
-    } 
+    def getUnvisited(h: LinkedHashSet[Edge[T,S]]) = h.find(v => !visited.contains(v.to))
     
-    */
-    
-    s.push(start)
+    collection.add(start)
     visited.add(start)
     f(start)
     
-    while (!s.isEmpty) {
-      println(s"stack = $s")
-      adjHash.get(s.top) match {
+    while (!collection.isEmpty) {
+      println(s"collection = $collection")
+      adjHash.get(collection.peek.get) match {
         case Some(x) => {
           val e = getUnvisited(x)
           if (e != None) {
             val v = e.get.to
             f(v)
             visited.add(v)
-            s.push(v)
+            collection.add(v)
           } else {
-            s.pop
+            collection.remove
           }
         }
         case _ =>
       }
-      /*
-      adjHash.get(s.top) match {
-        case x: Some[HashSet[Edge[T, S]]] => for (e <- x.get) {
-          if (!visited.contains(e.to)) {
-            f(e.to)
-            s.push(e.to)
-            visited.add(e.to)
-          
-          }
-        }
-        s.pop
-        case _ =>
-      }
-
-      */
-    }
-
+    } 
   }
 
 }
